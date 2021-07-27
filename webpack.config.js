d9715de3a9a4a7f0dda8828439bb2683d1a65f6c
Milana <milana.sadykova@protonmail.com>
2021-07-27 22:33:35 +0300

    start fluent-posts app

---
+++ webpack.config.js
@@ -0,0 +1,62 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { DefinePlugin } = require('webpack');
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    target: 'web',
+    entry: './src/main.jsx',
+    output: {
+        path: __dirname + '/build',
+        filename: 'bundle.js',
+        publicPath: '/',
+    },
+    resolve: {
+        extensions: ['.ts', '.tsx', '.css',]
+    },
+    devtool: 'inline-source-map',
+    module: {
+        rules: [
+            {
+                exclude: [
+                    /node_modules/,
+                ],
+                include: [
+                    path.resolve(__dirname, 'src'),
+                ],
+                test: /\.tsx$/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            ['@babel/preset-typescript'],
+                            ["@babel/preset-env"],
+                            ["@babel/preset-react"]
+                        ],
+                        plugins: ["@babel/plugin-proposal-class-properties"],
+                    }
+                }
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ],
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'public/index.html'
+        }),
+    ],
+    devServer: {
+        port: 3000,
+        contentBase: path.join(__dirname, 'build'),
+        publicPath: '/',
+        hot: true,
+        historyApiFallback: true,
+    }
+}
